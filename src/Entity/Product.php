3cb3001dc0ffdb77f937c92610ac99f9c20518b0
Łukasz Chruściel <lchrusciel@gmail.com>
2019-09-05 10:07:26 +0200

    [Product Catalog] Implement application feature

---
+++ src/Entity/Product.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ProductRepository")
+ */
+class Product
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $price;
+
+    public function __construct(string $name, int $price)
+    {
+        $this->name = $name;
+        $this->price = $price;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function getPrice(): ?int
+    {
+        return $this->price;
+    }
+}
--- tests/Behat/Application/ProductContext.php
+++ tests/Behat/Application/ProductContext.php
@@ -4,8 +4,48 @@ declare(strict_types=1);
 
 namespace App\Tests\Behat\Application;
 
+use App\Entity\Product;
+use App\Repository\ProductRepository;
+use Behat\Behat\Tester\Exception\PendingException;
 use Behat\Behat\Context\Context;
+use Doctrine\Common\Persistence\ObjectManager;
+use Webmozart\Assert\Assert;
 
 final class ProductContext implements Context
 {
+    /**
+     * @var ObjectManager
+     */
+    private $manager;
+    /**
+     * @var ProductRepository
+     */
+    private $productRepository;
+
+    public function __construct(ObjectManager $manager, ProductRepository $productRepository)
+    {
+        $this->manager = $manager;
+        $this->productRepository = $productRepository;
+    }
+
+    /**
+     * @When I add a :name product that costs :price USD
+     */
+    public function iAddAProductThatCostsUsd(string $name, float $price): void
+    {
+        $price = (int) $price * 100;
+
+        $product = new Product($name, $price);
+
+        $this->manager->persist($product);
+        $this->manager->flush();
+    }
+
+    /**
+     * @Then I should see a :name in my product catalog
+     */
+    public function iShouldSeeAInMyProductCatalog(string $name): void
+    {
+        Assert::notNull($this->productRepository->findOneBy(['name' => $name]));
+    }
 }
